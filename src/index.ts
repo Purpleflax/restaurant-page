e54983cdf5e1820a8043d8bbd1ff278ef01047ea
Jeffrey Reynolds <reynoldsjeffrey414@gmail.com>
2023-01-30 14:51:18 +0000

    Add beginning of page format

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import "./style.scss";
 
-console.log('Hello World, updated!');
-
-let a: number = 1;
-document.addEventListener('click', () => {
-  document.body.innerHTML = `Hello World, updated! ${a}`;
-  a++;
-});
+const body = document.body;
+const background = document.createElement("div");
+background.classList.add("background");
+body.appendChild(background);
+const title = document.createElement("h1");
+title.classList.add("title");
+title.innerText = "Food n' Stuff";
+body.appendChild(title);
+const contactButton = document.createElement("a");
+contactButton.classList.add("contact-button");
+contactButton.innerText = "Contact Us";
+contactButton.href = "../dist/contact.html";
+body.appendChild(contactButton);
+const menuButton = document.createElement("a");
+menuButton.classList.add("menu-button");
+menuButton.innerText = "Menu";
+menuButton.href = "../dist/menu.html";
+body.appendChild(menuButton);
+const aboutButton = document.createElement("a");
+aboutButton.classList.add("about-button");
+aboutButton.innerText = "About Us";
+aboutButton.href = "../dist/about.html";
+body.appendChild(aboutButton);
